c3198bb45fb6c3d445576a4869faefdcd07f791f
Mike <munkyu0813@gmail.com>
2018-06-10 22:55:45 -0400

    revamp main view

--- src/components/NoteView/NoteMain.js
+++ src/components/NoteView/NoteMain.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NoteMain = () => (
   <div className="note__main">
-    <h1>Welcome to Notes</h1>
     <img src="/img/notes.png" alt="" />
+    <h1>Welcome to Notes</h1>
+    <p>Capture your thoughts, stay inspired.</p>
+    <Link to={`/new`}>
+      <button>Take a note</button>
+    </Link>
   </div>
 );
 
